a9e9fb230262c32fdb38b590b5be3db8747f1b8e
mbashia <mbashiavictor@gmail.com>
2024-05-03 23:44:16 +0300

    added

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :betting_system, BettingSystem.Repo,
 config :betting_system, BettingSystemWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [port: 4500],
+  http: [port: 5700],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
